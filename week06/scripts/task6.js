12bed32a08baae67f6bcd625a554009248f5c921
lfacer <lfacer769@gmail.com>
2022-05-26 16:08:00 -0600

    semi-finished

--- week06/scripts/task6.js
+++ week06/scripts/task6.js
@@ -1,87 +1,46 @@
-const today = new Date();
+// Pikachu
+const output = (pikachu) => {
+    let articleDefault = document.createElement("article");
 
-let dayOfWeek;
-
-dayOfWeek = today.getDay();
-
-let message1;
-
-if (dayOfWeek >= 1 && dayOfWeek <=5) {
-    message1 = "Weekday";
-}
-
-else {
-    message1 = "Weekend";
-}
-
-let message2;
-switch (dayOfWeek) {
-    case 0:
-        message2 = "Sunday";
-        break;
-    case 1:
-        message2 = "Monday";
-        break;
-    case 2:
-        message2 = "Tuesday";
-        break;
-    case 3:
-        message2 = "Wednesday";
-        break;
-    case 4:
-        message2 = "Thursday";
-        break;
-    case 5:
-        message2 = "Friday";
-        break;
-    case 6:
-        message2 = "Saturday";
-        break;
-    default:
-        message2 = "Unknown - " + dayOfWeek
-        break;
-}
-
-document.querySelector("#message1").textContent = message1;
-document.querySelector("#message2").textContent = message2;
-
-
-const output = (pokemons) => {
-    pokemons.forEach((sprites) => {
-        let article = document.createElement("article");
-
-        let img1 = document.createElement("img");
-        img1.setAttribute("src", sprites.front_default);
-
-        let img2 = document.createElement("img");
-        img2.setAttribute("src", sprites.back_default);
+    let articleShiny = document.createElement("article");
     
-        article.appendChild(img1);
-        article.appendChild(img2);
+    let pikachuName = document.createElement("h3");
+    pikachuName.textContent = pikachu.species.name;
 
-        document.querySelector("#pokemons").appendChild(article);
-    });
+    let img1 = document.createElement("img");
+    img1.setAttribute("src", pikachu.sprites.front_default);
+
+    let img2 = document.createElement("img");
+    img2.setAttribute("src", pikachu.sprites.back_default);
+
+    let img3 = document.createElement("img");
+    img3.setAttribute("src", pikachu.sprites.front_shiny);
+
+    let img4 = document.createElement("img");
+    img4.setAttribute("src", pikachu.sprites.back_shiny);
+    
+    articleDefault.appendChild(pikachuName);
+    articleDefault.appendChild(img1);
+    articleDefault.appendChild(img2);
+    articleShiny.appendChild(img3);
+    articleShiny.appendChild(img4);
+
+    document.querySelector("#pikachu").appendChild(articleDefault);
+    document.querySelector("#pikachu").appendChild(articleShiny);
 };
 
 
-const getPokemon = async () => {
+const getPikachu = async () => {
     const respone = await fetch(
         "https://pokeapi.co/api/v2/pokemon/pikachu"
     );
     pokemonList = await respone.json();
     output(pokemonList);
 };
-getPokemon();
+getPikachu();
 
 
 const reset = () => {
-    document.querySelector("#pokemons").innerHTML = "";
+    document.querySelector("#pikachu").innerHTML = "";
 };
 
-// const pokemon1 = {
-//     pokemonName: "Eevee",
-//     pokemonType: "Normal",
-//     picture: "insert png here"
-// };
-
-// document.querySelector('#eevee').textContent = pokemon1;
